c1371c658f6a9b306a6b698893bd1b1106b052a5
root <root@vmi1263820.contaboserver.net>
2023-05-25 23:32:16 +0200

    update

--- project/settings.py
+++ project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-r(99a#r(5dm=9q%6kxr*j7&1d+k&q&i7rns@-b49692ip9o#)g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True 
+DEBUG = False 
 
 ALLOWED_HOSTS = ['5.189.171.130', 'localhost', 'http://www.viid.pro', 'viid.pro', 'www.viid.pro', '127.0.0.1']
 
