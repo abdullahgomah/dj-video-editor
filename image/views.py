03612778b2c54c514b44c3871ad1f0fdf4eb6c5f
abdullahgomah <djangodeveloper6@gmail.com>
2024-03-22 17:56:34 +0200

    edit

--- image/views.py
+++ image/views.py
@@ -6,6 +6,11 @@ import datetime
 from PIL import Image 
 import numpy as np 
 import os 
+import zipfile 
+from django.contrib import messages 
+from moviepy.editor import *
+from io import StringIO
+import uuid 
 
 # Create your views here.
 
@@ -69,3 +74,83 @@ def changesize(request):
     else:
         return render(request, 'image/changesize.html')
 
+
+def bulk_size_change(request): 
+    if request.POST: 
+        images = request.FILES.getlist('images-input') 
+        size = request.POST.get('size-input') 
+        width = 0 
+        height = 0 
+
+        if size is not None and size is not "": 
+            if size == 'instagram-story': 
+                width = 1080 
+                height = 1920 
+            elif size == 'facebook-landscape': 
+                width = 1280 
+                height = 720 
+            elif size == 'facebook-portrait': 
+                width = 720 
+                height = 1280 
+            elif size == 'square': 
+                width = 1080 
+                height = 1080 
+            else: 
+                ## send error message 
+                messages.add_message(request, messages.ERROR, message='يرجي اختيار حجم مناسب')
+                return redirect("image:bulk-change-size")
+
+        clips = []
+        for img in images: 
+            img_path = img.temporary_file_path() 
+            clip = ImageClip(img_path).set_position('center', 'center')
+            if clip.size[0] > clip.size[1]: 
+                clip = clip.resize(height=height)  
+            elif clip.size[0] == clip.size[1]: 
+                ## square case 
+                if height > width: 
+                    clip = clip.resize(width=width) 
+                else: 
+                    clip = clip.resize(height=height)  
+                
+            else: 
+                clip = clip.resize(width=width)
+            
+            clip = clip.set_position('center','center').set_duration(1) 
+            final_clip = CompositeVideoClip([clip], size=((width, height)))
+
+            clips.append(final_clip)
+        
+        print(clips) 
+
+        file_list = [] 
+        for clip in clips: 
+            filename = uuid.uuid4().hex[:10].upper()
+            file_path = './' + filename +'.png'
+            # clip.write_videofile(file_path, fps=1, codec='image/png') 
+            clip.write_videofile(file_path, fps=1, codec='png') 
+
+            file_list.append(file_path) 
+        
+
+        s = StringIO() 
+        zip_file_name= f'{uuid.uuid4().hex[:12]}.zip'
+        with zipfile.ZipFile(zip_file_name, 'w') as zip:
+
+            for f in file_list: 
+                zip.write(f) 
+        
+        print('Zipped Successfully')
+
+        with open(zip_file_name, 'rb') as file: 
+            resp = HttpResponse(file.read(), content_type='application/x-zip-compressed')
+            resp['Content-Disposition'] = 'attachment; filename=%s' % zip_file_name
+
+            return resp 
+        
+
+
+        return redirect("image:bulk-change-size")
+
+    context = {} 
+    return render(request, 'image/bulk-size-change.html', context)
